7a087d36560a212f81be8ec3cadf61d23571b2d9
Rishi Raj <rishiraj88+github@gmail.com>
2023-01-12 23:41:11 +0100

    Added comment and improved main() method code for TwoSum.

--- TwoSum.java
+++ TwoSum.java
@@ -5,30 +5,34 @@ import java.util.stream.Stream;
 
 public class TwoSum {
 
-    public int[] twoSum(int[] nums,int target) {
+    /*
+    Return 1-based positions of the two elements in the number array
+    whose sum equals the target value.
+     */
+    public static int[] twoSum(int[] nums, int target) {
         int a = 0;
-        int b = nums.length -1;
+        int b = nums.length - 1;
 
-        while(a <= b) {
+        while (a <= b) {
             int sum = nums[a] + nums[b];
 
-            if(sum > target){
+            if (sum > target) {
                 b -= 1;
-            }else if (sum < target) {
-                a +=1;
+            } else if (sum < target) {
+                a += 1;
             } else {
-                return new int[]{a+1, b+1};
+                return new int[]{a + 1, b + 1};
             }
         }
-        return new int[] {a+1,b+1};
+        return new int[]{a + 1, b + 1};
     }
 
-    // test for
-    // [2,7,11,15], 9
-    public static void main (String[] args) {
-        int[] naInput = {2,7,11,15};
+    public static void main(String[] args) {
+        // test for
+        // [2,7,11,15], 9
+        int[] naInput = {2, 7, 11, 15};
         int targetSum = 9;
 
-        System.out.println(toSum(naInput,targetSum));
+        System.out.println(Arrays.toString(twoSum(naInput, targetSum)));
     }
 }
